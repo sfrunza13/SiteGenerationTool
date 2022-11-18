712555911e5992886825526e31b41cdfe720dd77
rudychung <61764259+rudychung@users.noreply.github.com>
2022-11-17 21:10:13 -0500

    Make formatting changes

--- tests/test_ssj_edit_template.py
+++ tests/test_ssj_edit_template.py
@@ -3,6 +3,7 @@
 import pytest
 from ssj import SSJ  # pylint: disable=import-error
 
+
 def test_edit_template():
     """
     Test adding titles and paragraphs to template
@@ -11,9 +12,11 @@ def test_edit_template():
         "<h1>This is the title</h1>",
         "<p>this is the first test paragraph</p>",
         "<p>this is the second test paragraph</p>",
-        "<p>this is the third test paragraph</p>"
+        "<p>this is the third test paragraph</p>",
     ]
 
     SSJ.edit_template(SSJ, input_paragraphs, True)
     assert SSJ.template.find("<title>This is the title</title>")
-    assert SSJ.template.find("<body><h1>This is the title</h1><p>this is the first test paragraph</p><p>this is the second test paragraph</p><p>this is the third test paragraph</p></body>")
+    assert SSJ.template.find(
+        "<body><h1>This is the title</h1><p>this is the first test paragraph</p><p>this is the second test paragraph</p><p>this is the third test paragraph</p></body>"
+    )
